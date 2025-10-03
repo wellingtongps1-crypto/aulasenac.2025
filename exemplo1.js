bcfca18afc2be903748822952e951df2177425da
wellingtongps1-crypto <wellingtongps1@gmail.com>
2025-10-03 19:23:48 -0300

    Alteração feita no exemplo1
    Apaguei os comentários

--- exemplo1.js
+++ exemplo1.js
@@ -1,8 +1,3 @@
-//idade de eleitor
-// menor 16 não pode votar
-// 16,17 eleitor opcional
-// 18 eleitor obrigatório
-
 let idade = Number(prompt('Informe a idade'));
 if (idade<16){
     console.log('Não pode votar');
